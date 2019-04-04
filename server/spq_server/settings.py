97c20db17b77f89ca30cb2ef75910446d8e6fe82
mikelsr <mikelsr@pm.me>
2019-04-04 12:30:42 +0200

    Change storage engine from SQLite to MySQL

--- server/spq_server/settings.py
+++ server/spq_server/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'spq_server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "moviemanager",
+        "USER": "mm",
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators

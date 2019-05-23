9d95f073f92cd66b1a50fb8d15d6871981f40f2f
luisur <luis.urre@opendeusto.es>
2019-05-23 07:50:24 +0200

    Update views.py

--- server/moviemanager/views.py
+++ server/moviemanager/views.py
@@ -46,6 +46,15 @@ def get_query(request):
 @require_http_methods(["DELETE", "GET", "POST", "PUT"])
 @silk_profile(name='Handle user albums by title')
 def handle_album_by_title(request):
+
+    
+## @brief Create album
+# creates an album with the specified title associated to the user
+# @param request HTTP REST request with the title of the album and the user token
+# @param title title of the album
+# @return JSON containing album_id if all went well, error otherwise
+#
+ 
     """
     create_album creates an album with the specified title associated to the
     user
@@ -111,7 +120,16 @@ def handle_album_by_title(request):
 @require_http_methods(["DELETE", "GET", "POST"])
 @silk_profile(name='Handle user albums by ID')
 def handle_album(request, album_id):
-    """
+
+## @brief handle_album
+# Get an album, create it, add movies to it, remove movies from it or delete it
+# @param token token of the current session
+# @param album_id ID of the album
+# @param movie_id ID of the movie to add to or remove from the album
+# @return album if the request method is GET
+#
+   
+"""
     handle_album is used to:
         delete an album
         delete a movie from an album
@@ -386,6 +404,15 @@ def user_albums(request):
 @require_http_methods(["DELETE", "GET", "POST"])
 @silk_profile(name='Handle watched movies')
 def watched_movies(request):
+   
+
+## @brief watched_movies
+# Return a list of watched movvies of a user 
+# @param Request request containing token to identify the user with and the movie_id
+# @return list of movies if token was valid
+#
+
+
     """
     watched_movies returns the list of watched movies of a user
     :param request: request containing token to identify the user with and
